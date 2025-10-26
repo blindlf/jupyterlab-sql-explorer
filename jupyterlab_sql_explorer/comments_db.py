67e4757913e320fd0039e19bcef2fe94c572f070
Scott Liu <blindlf@hotmail.com>
2025-10-26 20:19:06 +0800

    Rename comments table column name to avoid using reversed words

--- jupyterlab_sql_explorer/comments_db.py
+++ jupyterlab_sql_explorer/comments_db.py
@@ -17,14 +17,14 @@ class Comments(Base):
     __tablename__ = 'comments'
 
     id = Column(Integer, comments_sequence, primary_key=True)
-    type = Column(Integer)
+    objtype = Column(Integer)
     dbid = Column(String(20))
-    schema = Column(String(100))
+    schname = Column(String(100))
     tabname = Column(String(100))
     colname = Column(String(100))
     memo = Column(String(500))
 
-    index_name = Index('idx', type, dbid, schema, tabname, colname)
+    index_name = Index('idx', objtype, dbid, schname, tabname, colname)
 
 def init(conn_str: str):
     global _conn_str
@@ -37,11 +37,11 @@ def get_conn_comments():
         engine = create_engine(_conn_str)
         with engine.connect() as conn:
             result = conn.execute(text('''
-            SELECT dbid, memo FROM comments 
+            SELECT dbid, memo FROM comments
             WHERE id in ( SELECT max(id) as id FROM comments
-                    WHERE type = :type
+                    WHERE objtype = :objtype
                     GROUP BY dbid )
-            '''), {"type":str(C_CONN)})
+            '''), {"objtype":str(C_CONN)})
             data={}
             for r in result.fetchall():
                 data[r[0]]=r[1]
@@ -54,11 +54,11 @@ def get_schema_comments(dbid: str):
         engine = create_engine(_conn_str)
         with engine.connect() as conn:
             result = conn.execute(text('''
-            SELECT schema, memo FROM comments
+            SELECT schname, memo FROM comments
             WHERE id in ( SELECT max(id) as id FROM comments
-                        WHERE type = :type and dbid = :dbid
-                        GROUP BY schema )
-            '''), {'type':str(C_SCHEMA), 'dbid':dbid})
+                        WHERE objtype = :objtype and dbid = :dbid
+                        GROUP BY schname )
+            '''), {'objtype':str(C_SCHEMA), 'dbid':dbid})
             data={}
             for r in result.fetchall():
                 data[r[0]]=r[1]
@@ -74,9 +74,9 @@ def get_table_comments(dbid: str, schema: str):
             result = conn.execute(text('''
             SELECT tabname, memo FROM comments
             WHERE id in ( SELECT max(id) as id FROM comments
-                        WHERE type = :type and dbid = :dbid and schema = :schema
+                        WHERE objtype = :objtype and dbid = :dbid and schname = :schname
                         GROUP BY tabname )
-            '''), {'type':str(C_TABLE), 'dbid':dbid, 'schema':schema})
+            '''), {'objtype':str(C_TABLE), 'dbid':dbid, 'schname':schema)
             
             for r in result.fetchall():
                 data[r[0]]=r[1]
@@ -91,9 +91,9 @@ def get_column_comments(dbid: str, schema: str, table: str):
             result = conn.execute(text('''
             SELECT colname, memo FROM comments
             WHERE id in ( SELECT max(id) as id FROM comments
-                        WHERE type = :type and dbid = :dbid and schema = :schema and tabname = :table
+                        WHERE objtype = :objtype and dbid = :dbid and schname = :schname and tabname = :tabname
                         GROUP BY colname )
-            '''), {'type':str(C_COLUMN), 'dbid':dbid, 'schema':schema, 'table':table})
+            '''), {'objtype':str(C_COLUMN), 'dbid':dbid, 'schname':schema, 'tabname':table})
             data={}
             for r in result.fetchall():
                 data[r[0]]=r[1]
@@ -104,7 +104,7 @@ def get_column_comments(dbid: str, schema: str, table: str):
 def set_comments(**args):
 
     param={
-        'type': args['type'],
+        'objtype': args['type'],
         'memo': args['comment'],
         'dbid': args['dbid']
     }
@@ -114,11 +114,11 @@ def set_comments(**args):
     if type==C_CONN:
         pass
     elif type==C_SCHEMA:
-        param.update({'schema': args['schema']})
+        param.update({'schname': args['schema']})
     elif type==C_TABLE:
-        param.update({'schema': args['schema'], 'tabname': args['table']})
+        param.update({'schname': args['schema'], 'tabname': args['table']})
     elif type==C_COLUMN:
-        param.update({'schema': args['schema'], 'tabname': args['table'], 'colname': args['column']})
+        param.update({'schname': args['schema'], 'tabname': args['table'], 'colname': args['column']})
     else:
         raise Exception('arg error')
 
